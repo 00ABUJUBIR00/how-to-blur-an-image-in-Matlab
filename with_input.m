e80239f3d43fecd7c1b47cb269ce34ad67e4e8c0
𝓐𝓫𝓾 𝓙𝓾𝓫𝓪𝓲𝓻 <168115520+00ABUJUBIR00@users.noreply.github.com>
2024-05-25 18:21:29 +0600

    Add files via upload

---
+++ with_input.m
@@ -0,0 +1,32 @@
+clc; clear all;
+
+x=imread('dog.jpg');
+[r,c,rgb]=size(x);
+disp('The row of the picture is:');
+disp(r);
+disp('The coulum of the picture is:');
+disp(c);
+
+ row_start = input('Please input first ROW of the subject region:');
+ 
+ row_finish = input('Please input last ROW of the subject region:');
+ 
+ column_start = input('Please input first COULUM of the subject region:');
+ 
+ column_finish = input('Please input last COULUM of the subject region:');
+ 
+Intensity = input('Please input the intensity of the blur:');
+ 
+ %bluring the whole image
+
+ region = x( 1 : r, 1 : c , :);
+ 
+ blurred_image = imgaussfilt( region, Intensity );  
+
+ figure; imshow(blurred_image);
+
+ %replacing the subject region with the blured region
+ 
+blurred_image(row_start :row_finish, column_start: column_finish,:)=x(row_start :row_finish, column_start: column_finish,:);
+
+figure; imshow(blurred_image);
